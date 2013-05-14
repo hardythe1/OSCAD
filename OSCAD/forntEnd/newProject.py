f7f2fa9163cc1e07013afafe404e5927a61b66b9
shambu-nd <shambund@gmail.com>
2013-05-14 20:27:19 +0530

    updated newproje.py on line no.268 to open the new terminal to select NPN/PNP in model builder

--- OSCAD/forntEnd/newProject.py
+++ OSCAD/forntEnd/newProject.py
@@ -265,7 +265,7 @@ class ProjectParam(template.MyTemplate):
     self.text.yview(END)
   # Call all pending idle tasks, without processing any other events.
     self.update_idletasks()
-    command=self.OSCAD_HOME+"/modelEditor/modelEditor.py " +self.projectName+".cir "
+    command="gnome-terminal -e \""+self.OSCAD_HOME+"/modelEditor/modelEditor.py " +self.projectName+".cir 1\""
     try:
         thread.start_new_thread(self.call_system,(command,))
     except Exception,err:
